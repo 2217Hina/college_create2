86dbc372a3a0a0517818ec951d7d31fe69a983e0
2217Hina <hina.funatsu@gmail.com>
2023-01-02 12:55:10 +0900

    migration

--- database/migrations/2023_01_02_033543_create_rooms_table.php
+++ database/migrations/2023_01_02_033543_create_rooms_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('room_num');
+            $table->integer('capacity');
+            $table->string('piano');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 };
